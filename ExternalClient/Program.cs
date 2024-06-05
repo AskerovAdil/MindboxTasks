47e516c6cbc004f098d955588972414dcfc654a0
AskerovAdil <Askerov121099@mail.ru>
2024-06-05 17:21:38 +0300

    feat: add external client

---
+++ ExternalClient/Program.cs
@@ -0,0 +1,23 @@
+﻿using ShapeLibrary.Models.Enums;
+using ShapeLibrary.Models.Factory;
+using ShapeLibrary.Models.Shapes;
+
+namespace ExternalClient
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var circle = ShapeFactory.CreateShape(ShapeType.Circle, 5.0);
+            var triangleArea = ShapeFactory.CreateShape(ShapeType.Triangle, 3.0, 4.0, 5.0);
+
+            var circleArea = circle.GetArea();
+            var triangleAreaa = triangleArea.GetArea();
+
+
+            Console.WriteLine(circleArea);
+            Console.WriteLine(triangleAreaa);
+
+        }
+    }
+}
